2cba397eb6d4c59d811ea155074c9701de868b79
asaleh33 <salehastro@gmail.com>
2019-02-26 22:48:02 +0100

    Testing the iterator class, besides, begin & end functions. All functions are working and gave the correct output

--- c++/main.cpp
+++ c++/main.cpp
@@ -12,22 +12,24 @@ int Treevalues[9] = {8,10,14,13,3,6,7,4,1};
 int main(int argc, char** argv) {
 
   BSTree<int,int> myTree;
+  std::pair<int,int> keyval;
 
   std::cout << "Before inserting numbers, print the tree in order:\n";
   myTree.TreeTraversal();
 
-  std::pair<int,int> keyval;
-
-  for(int i=0; i < 9; i++)
-  {
+  for(int i=0; i < 9; i++) {
     keyval.first = TreeKeys[i];
     keyval.second = Treevalues[i];
-    myTree.InsertKey(keyval);
-  }
+    myTree.InsertKey(keyval); }
 
   std::cout << "\nPrinting the original tree after inserting numbers -- print the tree in order: " << std::endl;
   myTree.TreeTraversal(); 
   std::cout << "\n "; 
+
+  /* Calling the class Iterator and the InsertKey function of the tree */
+  for (auto iter = myTree.begin(); iter != myTree.end(); ++iter){
+    keyval = *iter;
+    std::cout << keyval.first <<":"<< keyval.second << "\n"; }
 
   return 0;
 }
